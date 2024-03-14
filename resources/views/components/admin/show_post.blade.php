d08ce7e30953ea28389f1c6fdf24d12720c95bc5
UsmanGhani <usman.ghani@totalcaresupport.com>
2024-03-14 17:20:53 +0000

    Created Delete post data functionality.

--- resources/views/components/admin/show_post.blade.php
+++ resources/views/components/admin/show_post.blade.php
@@ -13,25 +13,36 @@
     <!-- Sidebar Navigation end-->
     <div class="page-content">
 
+        @if(session()->has('message'))
+            <div class="alert alert-danger">
+                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">x</button>
+                {{session()->get('message')}}
+            </div>
+        @endif
+
         <div class="mt-10 flex flex-col space-y-6 justify-center items-center">
             <h1 class="text-2xl text-white font-semibold">All Posts</h1>
             <table class="p-4 flex justify-center border border-gray-200 flex w-fit">
-                <tr class="flex space-x-20 bg-blue-200 my-4">
+                <tr class="flex space-x-32 bg-blue-200 my-4">
                     <td>Post title</td>
                     <td>Description</td>
                     <td>Post by</td>
                     <td>User type</td>
                     <td>Image</td>
+                    <td>Delete</td>
                 </tr>
                 @foreach($posts as $post)
 
-                    <tr class="flex space-x-20">
+                    <tr class="flex space-x-28">
                         <td>{{$post->title}}</td>
                         <td>{{$post->description}}</td>
                         <td>{{$post->name}}</td>
                         <td>{{$post->usertype}}</td>
                         <td>
-                            <img class="px-10" height="100px" width="150px" src="postimage/{{$post->image}}">
+                            <img class="px-4 flex" height="100px" width="150px" src="postimage/{{$post->image}}">
+                        </td>
+                        <td>
+                            <a href="{{url('delete_post',$post->id)}}" class="btn btn-danger" onclick="return confirm('Are you sure to delete')">Delete</a>
                         </td>
                     </tr>
                 @endforeach
